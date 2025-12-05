d19c4df7fccad84b26aad233eb9d99e4a576e665
gilotin <toshevnikolay@gmail.com>
2025-12-05 18:09:35 +0200

    fix: minor changes to css for better ui

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -4,12 +4,14 @@ import { Outlet } from "react-router";
 
 export function MainLayout() {
     return (
-        <div className={styles.appWrapper}>
-            <aside className={styles.sidebar}>{<Navigation />}</aside>
-            <main className={styles.main}>{<Outlet />}</main>
-            <footer className={styles.footer}>
-                <p>Test footer. All bla bla reserved. 2025</p>
-            </footer>
+        <div className={styles.layoutBackground}>
+            <div className={styles.appWrapper}>
+                <aside className={styles.sidebar}>{<Navigation />}</aside>
+                <main className={styles.main}>{<Outlet />}</main>
+                <footer className={styles.footer}>
+                    <p>Test footer. All bla bla reserved. 2025</p>
+                </footer>
+            </div>
         </div>
     );
 }
