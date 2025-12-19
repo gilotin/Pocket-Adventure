cf77a91ad119458335c10b6a431cbe04d8b9c1c0
gilotin <toshevnikolay@gmail.com>
2025-12-19 13:20:15 +0200

    feat(game-page): create GameLayout and css module

---
+++ src/app/pages/PocketAdventureGame/layout/GameLayout.tsx
@@ -0,0 +1,22 @@
+import styles from "./GameLayout.module.css";
+import { PocketAdventurePage } from "../PocketAdventurePage";
+
+export function GameLayout() {
+    return (
+        <div className={styles.layoutBackground}>
+            <div className={styles.gameWrapper}>
+                <header>
+                    <p>profiles</p>
+                    <p>Stats</p>
+                </header>
+                <main className={styles.mainGame}>
+                    <PocketAdventurePage />
+                </main>
+                <footer>
+                    <p>Test footer. All bla bla reserved. 2025</p>
+                    {/* placeholder */}
+                </footer>
+            </div>
+        </div>
+    );
+}
