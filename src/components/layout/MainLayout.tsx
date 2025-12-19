f1e7ac270b100eef78297d929b4b8daa9814dbf1
gilotin <toshevnikolay@gmail.com>
2025-12-19 11:22:13 +0200

    chore(mainlayout): removed some unused states

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { DetailsModal } from "../../app/pages/Home/components/detailsModal/DetailsModal";
 import { Navigation } from "../ui/navigation/Navigation";
 import styles from "./MainLayout.module.css";
 import { Outlet } from "react-router";
 
 export function MainLayout() {
-    const [isModalActive, setIsModalActive] = useState(false);
+    // const [isModalActive, setIsModalActive] = useState(false);
 
     return (
         <div className={styles.layoutBackground}>
@@ -17,12 +17,12 @@ export function MainLayout() {
                 </footer>
             </div>
             {}
-            {isModalActive && (
-                <>
-                    <div className={styles.backdrop}></div>
-                    <DetailsModal />
-                </>
-            )}
+            {/* {isModalActive && ( */}
+            <>
+                <div className={styles.backdrop}></div>
+                <DetailsModal />
+            </>
+            {/* )} */}
         </div>
     );
 }
