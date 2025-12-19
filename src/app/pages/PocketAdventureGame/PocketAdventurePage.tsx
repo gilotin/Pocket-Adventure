932ac2bea2b4af7350f7bfd63d85d06294fbec65
gilotin <toshevnikolay@gmail.com>
2025-12-19 13:21:41 +0200

    feat(game-page): add basic file structure

---
+++ src/app/pages/PocketAdventureGame/PocketAdventurePage.tsx
@@ -0,0 +1,11 @@
+import { GameNavigation } from "./components/navigation/GameNavigation";
+
+export function PocketAdventurePage() {
+    return (
+        <>
+            <GameNavigation />
+
+            <p>Hello from game page!!!</p>
+        </>
+    );
+}
