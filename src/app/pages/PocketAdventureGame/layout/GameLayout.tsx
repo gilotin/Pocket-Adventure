181a63766e579b21413734cbf19d848688c34b3a
gilotin <toshevnikolay@gmail.com>
2026-01-17 09:57:25 +0200

    feat(gameLayout): add session persistence across reloads

--- src/app/pages/PocketAdventureGame/layout/GameLayout.tsx
+++ src/app/pages/PocketAdventureGame/layout/GameLayout.tsx
@@ -1,6 +1,6 @@
 import styles from "./GameLayout.module.css";
 import { PocketAdventurePage } from "../PocketAdventurePage";
-import { useState, type JSX } from "react";
+import { useEffect, useState } from "react";
 import { Login } from "../auth/login/Login";
 import { Register } from "../auth/register/Register";
 import { AuthErrorHandler } from "../components/authErrorHandler/AuthErrorHandler";
@@ -8,13 +8,37 @@ import { AuthErrorHandler } from "../components/authErrorHandler/AuthErrorHandle
 export type AuthStatus = "unknown" | "guest" | "authenticated";
 type AuthMode = "login" | "register";
 type AuthErrorType = string | null;
+type AccountData = {
+    accountName: string;
+    profileName: string;
+    email: string;
+};
 
 export function GameLayout() {
     const [authStatus, setAuthStatus] = useState<AuthStatus>("unknown");
     const [authMode, setAuthMode] = useState<AuthMode>("login");
     const [authError, setAuthError] = useState<AuthErrorType>(null);
+    const [userData, setUserData] = useState<AccountData | null>(null);
 
     // To refactor it and make it more readable
+
+    useEffect(() => {
+        const storageData = localStorage.getItem("accountData");
+        if (!storageData) {
+            // Using unknown till I implement guest mode
+            setAuthStatus("unknown");
+            return;
+        }
+
+        try {
+            const parsedData: AccountData = JSON.parse(storageData);
+            setUserData(parsedData);
+            setAuthStatus("authenticated");
+        } catch (error) {
+            setUserData(null);
+            setAuthStatus("unknown");
+        }
+    }, []);
 
     return (
         <>
@@ -24,6 +48,7 @@ export function GameLayout() {
                         setIsAuthenticated={setAuthStatus}
                         setAuthMode={setAuthMode}
                         setAuthError={setAuthError}
+                        setUserData={setUserData}
                     />
                 ) : (
                     <Register setAuthMode={setAuthMode} setAuthError={setAuthError} />
@@ -32,7 +57,7 @@ export function GameLayout() {
                 <div className={styles.layoutBackground}>
                     <div className={styles.gameWrapper}>
                         <header className={styles.gameHeader}>
-                            <p>profiles</p>
+                            <p>Hello {userData?.accountName}!</p>
                             <p>Stats</p>
                         </header>
                         <main className={styles.mainGame}>
