3ca7c45a85378db783f0501032f2c10e7c950c86
gilotin <toshevnikolay@gmail.com>
2026-01-18 10:53:46 +0200

    feat(inventory): add basic inventory component

--- src/app/pages/PocketAdventureGame/features/inventory/Inventory.tsx
+++ src/app/pages/PocketAdventureGame/features/inventory/Inventory.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import styles from "./Inventory.module.css";
+
+type Item = {
+    name: string;
+    type: string;
+    quantity: number;
+};
+
 export function Inventory() {
+    const [inventoryItems, setInventoryItems] = useState<Item[] | null>(null);
+
+    useEffect(() => {
+        const data = localStorage.getItem("spawnedData");
+
+        try {
+            if (!data) {
+                return;
+            } else {
+                setInventoryItems(JSON.parse(data));
+            }
+
+            return;
+        } catch {
+            setInventoryItems(null);
+            return;
+        }
+    }, []);
+
+    // const inventorySpace = inventoryItems?.map((x) => {
+    //     return <div>
+
+    //     </div>;
+    // });
+
     return (
         <>
-            <p>this is inventory panel!</p>
+            <section className={styles.inventoryWrapper}>
+                <h3 className={styles.inventoryHeader}>Inventory Bag</h3>
+                <div className={styles.inventorySpace}></div>
+            </section>
         </>
     );
 }
