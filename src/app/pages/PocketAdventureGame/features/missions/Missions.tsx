b6bf944cc208c056e53fca9dcd6290f110e8bb66
gilotin <toshevnikolay@gmail.com>
2026-02-20 16:35:01 +0200

    feat(missions): add basic UI elements and data space

--- src/app/pages/PocketAdventureGame/features/missions/Missions.tsx
+++ src/app/pages/PocketAdventureGame/features/missions/Missions.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import { MissionNavigation } from "./missionNavigation/MissionNavigation";
+
 export function Missions() {
+    const [missionNavigation, setMissionNavigation] = useState("");
+
     return (
-        <>
-            <p>this is mission panel</p>
-        </>
+        <section>
+            <h1>Missions</h1>
+            <MissionNavigation setMissionNavigation={setMissionNavigation} />
+        </section>
     );
 }
