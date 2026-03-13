0483338187583d9c0bdfa47bc9a0361fd004c6c2
gilotin <toshevnikolay@gmail.com>
2026-03-14 00:14:46 +0200

    refactor(home-card): improve UI and UX

--- src/app/pages/Home/components/projectCard/HomeCard.tsx
+++ src/app/pages/Home/components/projectCard/HomeCard.tsx
@@ -1,8 +1,27 @@
-import type { HomePageSection } from "../../staticData/staticHomeData";
 import styles from "./HomeCard.module.css";
 
-type HomeCardProps = {
-    section: HomePageSection;
-};
+export default function HomeCard() {
+    return (
+        <article className={styles.cardWrapper}>
+            <h3 className={styles.header}>Project Name</h3>
+            <p className={styles.description}>
+                project descriptionproject descriptionproject description project description
+                project description project description
+                ddddddddddddddddddddddddddddddddddddddddddddd
+            </p>
+            <ul className={styles.technologies}>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
 
-export default function HomeCard(props: HomeCardProps) {}
+            <div className={styles.actions}>
+                <a className={styles.linkButton} href="#" target="_blank" rel="noopener noreferrer">
+                    View source
+                </a>
+                <a className={styles.linkButton} href="#" target="_blank" rel="noopener noreferrer">
+                    Live
+                </a>
+            </div>
+        </article>
+    );
+}
