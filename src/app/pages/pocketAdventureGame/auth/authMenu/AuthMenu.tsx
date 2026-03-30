6232c4b986de38155ccd05e09513a4e1cc65d3cf
gilotin <toshevnikolay@gmail.com>
2026-03-30 03:15:03 +0300

    fix(auth): improved html semantic structure

--- src/app/pages/pocketAdventureGame/auth/authMenu/AuthMenu.tsx
+++ src/app/pages/pocketAdventureGame/auth/authMenu/AuthMenu.tsx
@@ -10,28 +10,38 @@ export function AuthMenu({ handleAuthAction: handleAuthAction }: AuthProps) {
         <section className={styles.wrapper}>
             <h1 className={styles.menuHeader}>Pocket Adventure</h1>
             <ul className={styles.menuList}>
-                <button
-                    className={styles.menuButton}
-                    onClick={() => handleAuthAction("login")}
-                    id="login"
-                >
-                    Login
-                </button>
-                <button
-                    className={styles.menuButton}
-                    onClick={() => handleAuthAction("register")}
-                    id="register"
-                >
-                    Register
-                </button>
-                <button
-                    className={styles.menuButton}
-                    onClick={() => handleAuthAction("guest")}
-                    id="guest"
-                >
-                    Guest Login
-                </button>
+                <li className={styles.menuListItem}>
+                    <button
+                        className={styles.menuButton}
+                        onClick={() => handleAuthAction("login")}
+                        id="login"
+                    >
+                        Login
+                    </button>
+                </li>
+                <li className={styles.menuListItem}>
+                    <button
+                        className={styles.menuButton}
+                        onClick={() => handleAuthAction("register")}
+                        id="register"
+                    >
+                        Register
+                    </button>
+                </li>
+                <li className={styles.menuListItem}>
+                    <button
+                        className={styles.menuButton}
+                        onClick={() => handleAuthAction("guest")}
+                        id="guest"
+                    >
+                        Guest Login
+                    </button>
+                </li>
             </ul>
+            <footer className={styles.menuFooter}>
+                <p>This game is currently in development.</p>
+                <p> Nikolay Toshev 2026</p>
+            </footer>
         </section>
     );
 }
