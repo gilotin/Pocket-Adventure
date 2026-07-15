36a8bd74a79e89d0b16a0627386ba273bef3c16b
gilotin <toshevnikolay@gmail.com>
2026-07-15 11:12:24 +0300

    refactor(game): add returned itemTypes

--- src/app/pages/pocketAdventureGame/MockedData/TestItemGenerator.tsx
+++ src/app/pages/pocketAdventureGame/MockedData/TestItemGenerator.tsx
@@ -43,7 +43,7 @@ export function TestItemGenerator() {
         }
 
         if (itemType === "potion" || itemType === "material") {
-            itemType;
+            return itemType;
         } else {
             equipmentSlot = itemType as EquipmentType;
             itemType = "equipment";
